eeb9728b77497e4ffbd975343f03e5637e190c8f
shreyanshu shankar <shreyanshu.absentia@gmail.com>
2019-08-18 05:18:01 +0530

    added addd urls config

--- apis/urlshortner/UrlShortner.py
+++ apis/urlshortner/UrlShortner.py
@@ -1,18 +1,24 @@
 import sys
-sys.path.append('../')
-from config.Config import generateFirebaseObject
+import random
+import string
+sys.path.append('../../')
+from apis.config.Config import generateFirebaseObject
+from flask import Blueprint
 
 add_urls = Blueprint('add_urls', __name__)
 
-@add_urls.route('/v1/url/add', methods = ['POST'])
+@add_urls.route('/v1/url/add', methods = ['GET'])
 def addUrls():
     firebase = generateFirebaseObject()
     db = firebase.database()
     data = {
         "name": "Mortimer 'Morty' Smith"
     }
-    results = db.child("users").push(data)
-    return resu.status
+    ids = ''.join(random.choices(string.ascii_uppercase + string.digits + string.ascii_lowercase, k=7))
+    print(ids)
+    results = db.child("users").child(ids).set(data)
+    print(results)
+    return ids
 
 
 firebase = generateFirebaseObject()
--- app.py
+++ app.py
@@ -2,16 +2,18 @@ from flask import Flask
 import sys
 # from apis.test.test import sms_twilio
 # from apis.charts.bar.BarChart import bar_chart
-from apis.urlshortner import add_urls
+from apis.urlshortner.UrlShortner import add_urls
 
 app = Flask(__name__)
 app.register_blueprint(add_urls)
 # app.register_blueprint(bar_chart)
 
+
 @app.route('/')
 def hello_world():
     return 'test-api'
 
+
 if __name__ == '__main__':
     # app.run(debug = True, host = '0.0.0.0')
-    app.run(debug = True, port= 4000)
+    app.run(debug=True, port=4000)
