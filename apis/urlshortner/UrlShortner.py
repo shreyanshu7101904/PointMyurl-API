abab5aa8712720e4e2ec83bad19f82d92db661ea
shreyanshu shankar <shreyanshu.absentia@gmail.com>
2019-08-18 05:51:43 +0530

    added feature for adding urls based on secret keys

--- apis/urlshortner/UrlShortner.py
+++ apis/urlshortner/UrlShortner.py
@@ -3,28 +3,27 @@ import random
 import string
 sys.path.append('../../')
 from apis.config.Config import generateFirebaseObject
-from flask import Blueprint
-
+from flask import Blueprint, request, jsonify
 add_urls = Blueprint('add_urls', __name__)
 
-@add_urls.route('/v1/url/add', methods = ['GET'])
+@add_urls.route('/v1/url/add', methods = ['POST'])
 def addUrls():
     firebase = generateFirebaseObject()
     db = firebase.database()
-    data = {
-        "name": "Mortimer 'Morty' Smith"
-    }
-    ids = ''.join(random.choices(string.ascii_uppercase + string.digits + string.ascii_lowercase, k=7))
-    print(ids)
-    results = db.child("users").child(ids).set(data)
-    print(results)
-    return ids
+    data = request.get_json()
+    if 'secret-key' in request.headers:
+        ids = ''.join(random.choices(string.ascii_uppercase + string.digits + string.ascii_lowercase, k=7))
+        print(ids)
+        results = db.child("users").child(ids).set(data)
+        print(results)
+        return jsonify({
+        "response-code":200,
+        "url": "https://pointmyurl.ml/" + ids
+        })
+    else:
+        return jsonify({
+        "response_code": 400,
+        "error_message":"authentiction_failed"
+    })
 
 
-firebase = generateFirebaseObject()
-print(firebase)
-db = firebase.database()
-data = {
-    "name": "Mortimer 'Morty' Smith"
-}
-results = db.child("users").push(data)
